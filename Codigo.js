5a8d08afb3e2be366cad236b1df9bd723df7da86
Sebastian Sarmiento Mendoza <pituraapetezible@gmail.com>
2019-09-06 18:59:46 -0500

    Add files via upload

---
+++ Codigo.js
@@ -0,0 +1,6 @@
+alert ("Si funciona");
+document.write ("Hola estudiante!! ");
+var ruta = window.location;
+document.write ("Estás en ", ruta);
+console.log ("Este es un mensaje secreto");
+console.log (window);
