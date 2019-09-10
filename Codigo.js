f51023bbfe31e3dbe684b987e033cfc5e4dbc9d1
Sebastian Sarmiento Mendoza <pituraapetezible@gmail.com>
2019-09-10 15:01:39 -0500

    Update Codigo.js

--- Codigo.js
+++ Codigo.js
@@ -1,10 +1,3 @@
-alert ("Si funciona");
-document.write ("Hola estudiante!! ");
-var ruta = window.location;
-document.write ("Estás en ", ruta);
-console.log ("Este es un mensaje secreto");
-console.log (window);
-
 alert ("ça fonctionne");
 document.write ("Salut étudiant!! ");
 var ruta = window.location;
